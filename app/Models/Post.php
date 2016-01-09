3c134f7015b9188fbff40586bb495d356b0716e4
Braxton Fair <b@i9t.xyz>
2016-01-09 01:42:59 -0600

    Started work on the Posts model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,21 @@
 
 namespace I9T\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use SoftDeletes;
+
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
 
     /**
      * The attributes that should be mutated to dates.
@@ -16,4 +25,13 @@ class Post extends Model
      */
     protected $dates = ['deleted_at'];
 
+    /**
+     * Get the author of a specified post
+     *
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return DB::table('users')->where('id', $this->user_id);
+    }
 }
